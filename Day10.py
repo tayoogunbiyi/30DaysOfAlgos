51d06248ca9e91cf9442614fee1c87b410a449c3
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-06-11 08:30:00 +0100

    added comment to day10 solution

--- Day10.py
+++ Day10.py
@@ -1,6 +1,6 @@
 def pivotIndex(nums):
     right_sum = sum(nums)
-    left_sum = 0
+    left_sum = 0 # this variable computes the sum to the left on the fly.
 
     for i in range(len(nums)):
         right_sum -= nums[i]
