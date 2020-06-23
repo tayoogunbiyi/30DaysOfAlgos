3cf8419f3bacf67562b5cf8cb9051c37072b2933
Eyitayo Ogunbiyi <eyitayoogunbiyi@gmail.com>
2020-06-23 08:08:30 +0100

    updated day 22 solution

--- Day22.py
+++ Day22.py
@@ -12,6 +12,33 @@ def reverseSentence(arr):
     else:
       wordEnd = i
   
-  string = [item for sublist in string for item in sublist]
+  string = [ch for substring in string for ch in substring]
 
   return string   
+
+
+
+# Approach 2 
+def reverse_arr(arr,start,end):
+      while start < end:
+        arr[start],arr[end] = arr[end],arr[start]
+        start+=1
+        end -= 1
+
+def reverse_words(arr):
+  reverse_arr(arr,0,len(arr)-1)
+  start = 0
+  for i in range(0,len(arr)):
+    if arr[i] == ' ':
+      reverse_arr(arr,start,i-1)
+      start = i
+      if start < len(arr) and arr[start] == ' ':
+        start +=1
+  if start < len(arr):
+    reverse_arr(arr,start,len(arr)-1)
+  
+  return arr
+
+print(reverse_words(['h','i',' ','t','h','e','r','e']))
+
+['t', 'h', 'e', 'r', 'e', ' ', 'h', 'i']
